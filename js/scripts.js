99d4ef676695a0bc8357eec26e8f8f3ca8cb7b64
Maximiliano <bboy1958.ml@gmail.com>
2021-07-21 21:20:17 -0300

    definimos variables light-mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,36 @@
-const ul = document.getElementById('ul');
+const ul = document.getElementById("ul");
 
-ul.addEventListener('click', (e) => {
+ul.addEventListener("click", (e) => {
     for (let link of ul.children) {
-        link.querySelector('a').classList.remove('active')
+        link.querySelector("a").classList.remove("active");
     }
-    e.target.classList.add('active')
-})
+    e.target.classList.add("active");
+});
 
 /* acvtive mobile nav */
 
-const mobileNav = document.querySelector('.mobile-nav')
+const mobileNav = document.querySelector(".mobile-nav");
 
-mobileNav.addEventListener('click', (e)=>{
-    for(let i of mobileNav.children){
-        i.classList.remove('active')
+mobileNav.addEventListener("click", (e) => {
+    for (let i of mobileNav.children) {
+        i.classList.remove("active");
     }
-    e.target.classList.add('active')
-})
+    e.target.classList.add("active");
+});
 
 /*end acvtive mobile nav */
 
 /* ====== HEADER SHOW/HIDE ====== */
-const header = document.querySelector('#mobile-header')
+const header = document.querySelector("#mobile-header");
 
-let scrollShow = window.scrollY
+let scrollShow = window.scrollY;
 
-document.addEventListener('scroll', (e) => {
-    let scrollNow = window.scrollY
-    if(scrollNow > scrollShow){
-        header.style.top = '-48px'
-    }else{
-        header.style.top = '0px'
+document.addEventListener("scroll", (e) => {
+    let scrollNow = window.scrollY;
+    if (scrollNow > scrollShow) {
+        header.style.top = "-48px";
+    } else {
+        header.style.top = "0px";
     }
-    scrollShow = scrollNow
-})
-
-console.log(header);
+    scrollShow = scrollNow;
+});
