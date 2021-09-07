2cdb7462ce121b91f99c2f36977d0cf48894435a
Maximiliano <bboy1958.ml@gmail.com>
2021-09-07 18:13:46 -0300

    add tag loader

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 /* ========== Pre-Loader ========== */
 window.addEventListener("load", function () {
     const $loader = document.querySelector(".container_loader");
-
-    $loader.classList.add("fade");
+    setTimeout(function () {
+        $loader.classList.add("fade");
+    }, 2000);
 });
 /* ========== End Pre-Loader ========== */
 
@@ -54,14 +55,16 @@ const sr = ScrollReveal({
 sr.reveal(`.logo`, {
     origin: "left",
     reset: false,
-    delay: 300,
+    delay: 2300,
+    useDelay: "once",
 });
 
 sr.reveal(`.nav_link_desk`, {
     origin: "right",
     interval: 200,
     reset: false,
-    delay: 500,
+    delay: 2500,
+    useDelay: "once",
 });
 
 sr.reveal(`.link-icon`, {
@@ -85,13 +88,15 @@ sr.reveal(`.hello, .welcome`, {
     origin: "bottom",
     interval: 300,
     reset: true,
-    delay: 1200,
+    delay: 3200,
+    useDelay: "once",
 });
 
 sr.reveal(`.name`, {
     duration: 1500,
     distance: "0px",
     opacity: 0,
-    delay: 300,
+    delay: 2300,
     reset: false,
+    useDelay: "once",
 });
